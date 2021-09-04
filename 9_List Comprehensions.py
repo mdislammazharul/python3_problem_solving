44d24b40673ab7dc488e4e4bda074871d648930f
mdislammazharul <mazharul.py@gmail.com>
2021-09-04 19:49:22 +0600

    list

---
+++ 9_List Comprehensions.py
@@ -0,0 +1,13 @@
+x = int(input())
+y = int(input())
+z = int(input())
+n = int(input())
+
+array = []
+for i in range(x+1):
+    for j in range(y+1):
+        for k in range(z+1):
+            if i+j+k != n:
+                array.append([i, j, k])
+
+print(array)
